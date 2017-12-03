1412bfecdc533b72c530ca3bfb1e588446bae1bc
Gyuhyeon <mark3236@naver.com>
2017-12-04 03:20:30 +0900

    fix routes broken during migration

--- public/assets/js/naverjob.js
+++ public/assets/js/naverjob.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
         // process the form
         $.ajax({
             type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : 'unsubscribe', // the url where we want to POST
+            url         : 'naverjob/unsubscribe', // the url where we want to POST
             data        : formData, // our data object
             dataType    : 'json', // what type of data do we expect back from the server
                         encode          : true
